fd2ff09f028a6d5bdb29347bd108d49abce0e02f
edlingao <sonido244@gmail.com>
2019-12-27 09:08:13 -0600

    Style(Groups): Improving Groups section frontend

--- src/DOM.js
+++ src/DOM.js
@@ -1,11 +1,18 @@
 const Dom = () => {
+    setEventListeners  = ()=>{
+        const addGroup = document.querySelector('#add-group');
+        const groupContainer = document.querySelector('#group-container');
+        addGroup.addEventListener('click', ()=>{
+            groupContainer.appendChild(drawGroup({groupName: 'Group Name'}));
+        });
+    },
     drawGroup = (group)=>{
         const container = document.createElement('div');
         const controlContainer = document.createElement('div');
         const editButton = document.createElement('button');
         const deleteButton = document.createElement('button');
         const groupName = document.createElement('h1');
-
+        
         groupName.innerHTML = group.groupName;
 
         editButton.innerHTML = `<i class="material-icons">create</i>`;
@@ -15,17 +22,21 @@ const Dom = () => {
         deleteButton.classList.add('delete');
         
         controlContainer.classList.add('control-container');
-        container.classList.add('group-continer');
+        container.classList.add('group');
 
         controlContainer.appendChild(editButton);
         controlContainer.appendChild(deleteButton);
 
         container.appendChild(groupName);
         container.appendChild(controlContainer);
+        container.tabIndex = -1;
         
         return container;
     },
     drawActivity = (activity) => {
         
     }
+    return{setEventListeners, drawGroup, drawActivity}
 }
+
+exports.Dom = Dom;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+const domFunctions = require('./DOM');
+const dom = domFunctions.Dom();
+const main = ()=>{
+  console.log(dom.setEventListeners());
 
+ 
+}
+
+main();
