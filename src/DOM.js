dcec122790b63827687b1bc1b0f07cf92a31652b
edlingao <sonido244@gmail.com>
2019-12-27 22:03:38 -0600

    feat(Group form): Added a form windows that can move freely, front end only, status: Working

--- src/DOM.js
+++ src/DOM.js
@@ -1,9 +1,16 @@
 const Dom = () => {
     setEventListeners  = ()=>{
         const addGroup = document.querySelector('#add-group');
+        const groupForm = document.querySelector('#group-form');
+        const addGroupForm = document.querySelector('#add-group-form');
         const groupContainer = document.querySelector('#group-container');
+
         addGroup.addEventListener('click', ()=>{
-            groupContainer.appendChild(drawGroup({groupName: 'Group Name'}));
+            groupForm.classList.remove('hidden');
+        });
+        addGroupForm.addEventListener('click', ()=>{
+            groupForm.classList.add('hidden');
+            groupContainer.appendChild(drawGroup({groupName: 'New Group', id: 1}))
         });
     },
     drawGroup = (group)=>{
@@ -30,11 +37,44 @@ const Dom = () => {
         container.appendChild(groupName);
         container.appendChild(controlContainer);
         container.tabIndex = -1;
+        container.id = group.id;
         
         return container;
     },
     drawActivity = (activity) => {
+        //Universal containers
+        const container = document.createElement('div');
+        const controlsContainer = document.createElement('div');
+        const descriptionContainer = document.createElement('div');
+        const header = document.createElement('header');
+
+        //Interactive containers
+        const important = document.createElement('button');
+        const completed = document.createElement('button');
+        const deleteButton = document.createElement('button');
+
+        //Text containers
+        const activityTitle = document.createElement('h1');
+        const activityDescription = document.createElement('p');
+        const starOutline = document.createElement('i');
+        const starFill = document.createElement('i');
+        const checkboxOutline = document.createElement('i');
+        const checkboxFill = document.createElement('i');
+        const close = document.createElement('i');
+
+        container.classList.add('activity-container');
+        controlsContainer.classList.add('activity-controls-container');
+        descriptionContainer.classList.add('activity-description-container');
+        header.classList.add('activity-header');
+
+        important.classList.add('important-button');
+        completed.classList.add('completed-button');
+        deleteButton.classList.add('delete-button');
+
+        activityTitle.classList.add('acitivity-title');
+        activityDescription.classList.add('activity-description');
         
+
     }
     return{setEventListeners, drawGroup, drawActivity}
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
 const domFunctions = require('./DOM');
 const dom = domFunctions.Dom();
-const main = ()=>{
+const interact = require('interactjs');
+
+ main = ()=>{
+
+  interact('#form-header').draggable(
+    {
+      inertia: false,
+      modifiers: [
+        interact.modifiers.restrictRect({
+          restriction: 'parent',
+          endOnly: true
+        })
+      ],
+      autoScroll: true,
+      onmove: dragMoveListener
+    }
+  );
+
   console.log(dom.setEventListeners());
 
  
 }
+const dragMoveListener  = (event) => {
+  var target = event.target.parentNode
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
 
+  // translate the element
+  target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
 main();
