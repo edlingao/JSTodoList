5105ad37205221393948e07fab5f2915fd33b1cf
edlingao <sonido244@gmail.com>
2020-01-23 00:05:14 -0600

    fix(style):Fixing code style according to linter

--- src/DOM.js
+++ src/DOM.js
@@ -1,160 +1,156 @@
 const DOM = () => {
-    const drawGroup = (group, groupContainer) => {
-      const container = document.createElement("div");
-      const controlContainer = document.createElement("div");
-      const deleteButton = document.createElement("button");
-      const groupName = document.createElement("h1");
-      const activityContainer = document.querySelector('#activities-container');
-      const idHolder = document.querySelector('.ID-holder');
-      const activityTitle = document.querySelector('#activities-title');
-      groupName.id = `group_title_${group.id}`;
-      groupName.innerHTML = group.groupName;
+  const drawGroup = (group, groupContainer) => {
+    const container = document.createElement("div");
+    const controlContainer = document.createElement("div");
+    const deleteButton = document.createElement("button");
+    const groupName = document.createElement("h1");
+    const activityContainer = document.querySelector("#activities-container");
+    const idHolder = document.querySelector(".ID-holder");
+    const activityTitle = document.querySelector("#activities-title");
+    groupName.id = `group_title_${group.id}`;
+    groupName.innerHTML = group.groupName;
 
+    deleteButton.innerHTML = `<i class="material-icons">delete_sweep</i>`;
+    deleteButton.classList.add("delete");
 
-      deleteButton.innerHTML = `<i class="material-icons">delete_sweep</i>`;
-      deleteButton.classList.add("delete");
+    controlContainer.classList.add("control-container");
+    container.classList.add("group");
 
-      controlContainer.classList.add("control-container");
-      container.classList.add("group");
+    controlContainer.appendChild(deleteButton);
 
-      controlContainer.appendChild(deleteButton);
+    container.appendChild(groupName);
+    container.appendChild(controlContainer);
+    container.tabIndex = -1;
+    container.id = `group_${group.id}`;
 
-      container.appendChild(groupName);
-      container.appendChild(controlContainer);
-      container.tabIndex = -1;
-      container.id = `group_${group.id}`;
-      
-
-      container.addEventListener('click', ()=>{
-        clearContainer(activityContainer);
-        activityTitle.innerText = group.groupName;
-        idHolder.id = group.id;
-        group.showActivities().forEach(activity => {
-          activityContainer.appendChild(drawActivity(activity, group));
-        });
+    container.addEventListener("click", () => {
+      clearContainer(activityContainer);
+      activityTitle.innerText = group.groupName;
+      idHolder.id = group.id;
+      group.showActivities().forEach(activity => {
+        activityContainer.appendChild(drawActivity(activity, group));
       });
-      deleteButton.addEventListener('click', ()=>{
-        deleteGroup(group);
-        groupContainer.deleteActivity(group.id);
-      });
-      return container;
-    };
-    const drawActivity = (activity, group) => {
-      //Universal containers
-      const container = document.createElement("div");
-      const controlsContainer = document.createElement("div");
-      const descriptionContainer = document.createElement("div");
-      const header = document.createElement("header");
+    });
+    deleteButton.addEventListener("click", () => {
+      deleteGroup(group);
+      groupContainer.deleteActivity(group.id);
+    });
+    return container;
+  };
+  const drawActivity = (activity, group) => {
+    //Universal containers
+    const container = document.createElement("div");
+    const controlsContainer = document.createElement("div");
+    const descriptionContainer = document.createElement("div");
+    const header = document.createElement("header");
 
-      //Interactive containers
-      const important = document.createElement("button");
-      const completed = document.createElement("button");
-      const deleteButton = document.createElement("button");
+    //Interactive containers
+    const important = document.createElement("button");
+    const completed = document.createElement("button");
+    const deleteButton = document.createElement("button");
 
-      //Text containers
-      const activityTitle = document.createElement("h1");
-      const activityDescription = document.createElement("p");
-      const activityDate = document.createElement('p');
-      const star = document.createElement("i");
-      const checkBox = document.createElement("i");
-      const close = document.createElement("i");
+    //Text containers
+    const activityTitle = document.createElement("h1");
+    const activityDescription = document.createElement("p");
+    const activityDate = document.createElement("p");
+    const star = document.createElement("i");
+    const checkBox = document.createElement("i");
+    const close = document.createElement("i");
 
-      container.classList.add("activity-container");
-      controlsContainer.classList.add("activity-controls-container");
-      descriptionContainer.classList.add("activity-description-container");
-      header.classList.add("activity-header");
+    container.classList.add("activity-container");
+    controlsContainer.classList.add("activity-controls-container");
+    descriptionContainer.classList.add("activity-description-container");
+    header.classList.add("activity-header");
 
-      important.classList.add("important-button");
-      completed.classList.add("completed-button");
-      deleteButton.classList.add("delete-button");
+    important.classList.add("important-button");
+    completed.classList.add("completed-button");
+    deleteButton.classList.add("delete-button");
 
-      activityTitle.classList.add("acitivity-title");
-      activityDescription.classList.add("activity-description");
-      activityDate.classList.add('activity-date');
+    activityTitle.classList.add("acitivity-title");
+    activityDescription.classList.add("activity-description");
+    activityDate.classList.add("activity-date");
 
-      star.classList.add("material-icons");
-      checkBox.classList.add("material-icons");
-      close.classList.add("material-icons");
+    star.classList.add("material-icons");
+    checkBox.classList.add("material-icons");
+    close.classList.add("material-icons");
 
-      star.innerHTML = "star_border";
-      checkBox.innerHTML = "check_box_outline_blank";
-      close.innerHTML = "clear";
+    star.innerHTML = "star_border";
+    checkBox.innerHTML = "check_box_outline_blank";
+    close.innerHTML = "clear";
 
-      activityTitle.innerText = activity.title;
-      activityDescription.innerText = activity.description;
-      activityDate.innerText = activity.dueDate;
+    activityTitle.innerText = activity.title;
+    activityDescription.innerText = activity.description;
+    activityDate.innerText = activity.dueDate;
 
-      important.appendChild(star);
-      completed.appendChild(checkBox);
-      deleteButton.appendChild(close);
+    important.appendChild(star);
+    completed.appendChild(checkBox);
+    deleteButton.appendChild(close);
 
-      controlsContainer.appendChild(important);
-      controlsContainer.appendChild(completed);
-      controlsContainer.appendChild(deleteButton);
+    controlsContainer.appendChild(important);
+    controlsContainer.appendChild(completed);
+    controlsContainer.appendChild(deleteButton);
 
-      header.appendChild(activityTitle);
-      header.appendChild(controlsContainer);
-      header.appendChild(activityDate);
+    header.appendChild(activityTitle);
+    header.appendChild(controlsContainer);
+    header.appendChild(activityDate);
 
-      descriptionContainer.appendChild(activityDescription);
+    descriptionContainer.appendChild(activityDescription);
 
-      container.appendChild(header);
-      container.appendChild(descriptionContainer);
-      container.id = `activity_${activity.id}`;
+    container.appendChild(header);
+    container.appendChild(descriptionContainer);
+    container.id = `activity_${activity.id}`;
 
-      if(activity.important) 
-        star.innerHTML = toggleSymbols(star);
-      if(activity.completed) 
-        checkBox.innerHTML = toggleSymbols(checkBox);
+    if (activity.important) star.innerHTML = toggleSymbols(star);
+    if (activity.completed) checkBox.innerHTML = toggleSymbols(checkBox);
 
-      important.addEventListener("click", () => {
-        star.innerHTML = toggleSymbols(star);
-        activity.important = !activity.important;
-      });
-      completed.addEventListener("click", () => {
-        checkBox.innerHTML = toggleSymbols(checkBox);
-        activity.completed = !activity.completed;
-      });
-      deleteButton.addEventListener('click', ()=>{
-        group.deleteActivity(activity.id);
-        deleteActivity(activity);
-      });
-      
-      return container;
-    };
-    const toggleSymbols = (action) => {
-      switch (action.innerHTML) {
-        case "star":
-          return "star_border";
+    important.addEventListener("click", () => {
+      star.innerHTML = toggleSymbols(star);
+      activity.important = !activity.important;
+    });
+    completed.addEventListener("click", () => {
+      checkBox.innerHTML = toggleSymbols(checkBox);
+      activity.completed = !activity.completed;
+    });
+    deleteButton.addEventListener("click", () => {
+      group.deleteActivity(activity.id);
+      deleteActivity(activity);
+    });
 
-        case "star_border":
-          return "star";
+    return container;
+  };
+  const toggleSymbols = action => {
+    switch (action.innerHTML) {
+      case "star":
+        return "star_border";
 
-        case "check_box":
-          return "check_box_outline_blank";
+      case "star_border":
+        return "star";
 
-        case "check_box_outline_blank":
-          return "check_box";
+      case "check_box":
+        return "check_box_outline_blank";
 
-        default:
-          break;
-      }
-    };
-    const deleteGroup = (group) =>{
-        
-        const groupContainer = document.querySelector(`#group_${group.id}`);
-        groupContainer.classList.add('fade-out');
-        setTimeout(()=> groupContainer.remove(), 200);
-    };
-    const deleteActivity = (activity) =>{
-      const activityContainer = document.querySelector(`#activity_${activity.id}`);
-      activityContainer.classList.add('fade-out');
-      setTimeout(()=> activityContainer.remove(), 200);
+      case "check_box_outline_blank":
+        return "check_box";
 
-    };
-    const clearContainer = (container)=>{
-      container.innerHTML = '';
-    }; 
+      default:
+        break;
+    }
+  };
+  const deleteGroup = group => {
+    const groupContainer = document.querySelector(`#group_${group.id}`);
+    groupContainer.classList.add("fade-out");
+    setTimeout(() => groupContainer.remove(), 200);
+  };
+  const deleteActivity = activity => {
+    const activityContainer = document.querySelector(
+      `#activity_${activity.id}`
+    );
+    activityContainer.classList.add("fade-out");
+    setTimeout(() => activityContainer.remove(), 200);
+  };
+  const clearContainer = container => {
+    container.innerHTML = "";
+  };
 
   return { drawGroup, drawActivity };
 };
--- src/activity.js
+++ src/activity.js
@@ -1,9 +1,19 @@
-let Activity = ( id, title, description, important = false, completed = false, dueDate) => {
-    if(title == '') title = 'New activity';
-    if(description == '') description = 'Description goes here';
-    if(dueDate == '') dueDate = new Date().toJSON().slice(0,10).replace(/-/g,'-');
-    return { id, title, description, important, completed, dueDate};
-  };
+let Activity = (
+  id,
+  title,
+  description,
+  important = false,
+  completed = false,
+  dueDate
+) => {
+  if (title == "") title = "New activity";
+  if (description == "") description = "Description goes here";
+  if (dueDate == "")
+    dueDate = new Date()
+      .toJSON()
+      .slice(0, 10)
+      .replace(/-/g, "-");
+  return { id, title, description, important, completed, dueDate };
+};
 
 export { Activity };
-
